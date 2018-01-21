988b4ac7bb54f485574872792a832fb66f5374ed
Amanda Bromberg <7b4571c5@opayq.com>
2018-01-21 18:30:51 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -3,5 +3,19 @@
 $(document).ready(function(){
 
 // call functions here
+getIt();
+frameIt();
 
 });
+
+
+function getIt() {
+  $("p").on("click", function() {alert("Hey!")})
+}
+
+
+function frameIt() {
+  $("img").on("load", img.addClass("tasty"))
+}
+
+///above is incorrect
